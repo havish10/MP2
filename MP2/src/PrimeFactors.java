400c2e8007a0b62eeafc3510a2c368df64e70dd6
havish10 <netlahavish@gmail.com>
2019-02-07 18:07:43 -0500

    Update PrimeFactors.java

--- MP2/src/PrimeFactors.java
+++ MP2/src/PrimeFactors.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 
+/**
+ * finds the prime factors
+ * 
+ * @author hnetla8053
+ *
+ */
 public class PrimeFactors {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int counter = 2;
+		int num = 0;
+		
+		System.out.println("Enter a number: ");
+		num = scanner.nextInt();
+		
+		while(counter <= num) {
+			if(num % counter == 0) {
+				System.out.println(counter);
+				num /= counter;
+			} else {
+				counter += 1;
+			}
+		}
+		scanner.close();
 
+	}
 }
